52c749727245860816b5af4d4e0f44906c4fbbbc
DESKTOP-GO7PEOM\DELL <fabriciolugao@gmail.com>
2025-05-07 20:48:47 -0300

    Ajustes estrutura

--- src/App.jsx
+++ src/App.jsx
@@ -1,52 +1,19 @@
 import React, { useEffect, useState } from 'react'
-import axios from 'axios'
-import { Link } from 'react-router-dom'
 
-const BASE_ID = 'appqkjRYvyIwSHLR7'
-const TABLE_NAME = 'locacoes'
-const API_URL = `https://api.airtable.com/v0/${BASE_ID}/${TABLE_NAME}`
-const TOKEN = 'SUA_API_KEY_AQUI'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import NewPage from './pages/NewPage'
+import EditPage from './pages/EditPage'
+import LocacoesPage from './pages/LocacoesPage'
 
 function App() {
-  const [locacoes, setLocacoes] = useState([])
-
-  useEffect(() => {
-    axios.get(API_URL, {
-      headers: {
-        Authorization: `Bearer ${TOKEN}`
-      }
-    })
-    .then(res => {
-      setLocacoes(res.data.records)
-    })
-    .catch(err => console.error(err))
-  }, [])
-
   return (
-    <div>
-      <h1>Locações</h1>
-      <Link to="/novo">Nova Locação</Link>
-      <table border="1">
-        <thead>
-          <tr>
-            <th>ID</th>
-            <th>Título</th>
-            <th>Ações</th>
-          </tr>
-        </thead>
-        <tbody>
-          {locacoes.map(loc => (
-            <tr key={loc.id}>
-              <td>{loc.id}</td>
-              <td>{loc.fields.titulo}</td>
-              <td>
-                <Link to={`/editar/${loc.id}`}>Editar</Link>
-              </td>
-            </tr>
-          ))}
-        </tbody>
-      </table>
-    </div>
+  <BrowserRouter>
+    <Routes>
+      <Route path="/" element={<LocacoesPage />} />
+      <Route path="/novo" element={<NewPage />} />
+      <Route path="/editar/:id" element={<EditPage />} />
+    </Routes>
+  </BrowserRouter>
   )
 }
 
---
+++ src/pages/LocacoesPage.jsx
@@ -0,0 +1,53 @@
+import React, { useEffect, useState } from 'react'
+import axios from 'axios'
+import { Link } from 'react-router-dom'
+
+const BASE_ID = 'appqkjRYvyIwSHLR7'
+const TABLE_NAME = 'locacoes'
+const API_URL = `https://api.airtable.com/v0/${BASE_ID}/${TABLE_NAME}`
+const TOKEN = 'patbG5cw2lF4cS5xW.888c2f44b0799d47fe3c96b555aac12f8fcf910c898c5b4ccde4467cfb2f87bf'
+
+function LocacoesPage() {
+  const [locacoes, setLocacoes] = useState([])
+
+  useEffect(() => {
+    axios.get(API_URL, {
+      headers: {
+        Authorization: `Bearer ${TOKEN}`
+      }
+    })
+    .then(res => {
+      setLocacoes(res.data.records)
+    })
+    .catch(err => console.error(err))
+  }, [])
+
+  return (
+    <div>
+      <h1>Locações</h1>
+      <Link to="/novo">Nova Locação</Link>
+      <table border="1">
+        <thead>
+          <tr>
+            <th>ID</th>
+            <th>Título</th>
+            <th>Ações</th>
+          </tr>
+        </thead>
+        <tbody>
+          {locacoes.map(loc => (
+            <tr key={loc.id}>
+              <td>{loc.id}</td>
+              <td>{loc.fields.titulo}</td>
+              <td>
+                <Link to={`/editar/${loc.id}`}>Editar</Link>
+              </td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </div>
+  )
+}
+
+export default LocacoesPage
